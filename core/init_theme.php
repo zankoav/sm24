b0c8b7132b9ce13d89054036465d8d2992ddee6f
Alexandr <aleksandr.zanko@enway.com>
2020-06-08 09:32:00 +0300

    Remove block-library css

--- core/init_theme.php
+++ core/init_theme.php
@@ -4,7 +4,7 @@
 		exit();
 	}
 
-	add_action( 'after_setup_theme', 'sm24_theme_init' );
+	
 
 	function sm24_theme_init() {
 		/**
@@ -39,4 +39,14 @@
 		 */
 		// add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
 
-	}
+    }
+
+    add_action( 'after_setup_theme', 'sm24_theme_init' );
+    
+    function smartwp_remove_wp_block_library_css(){
+        wp_dequeue_style( 'wp-block-library' );
+        wp_dequeue_style( 'wp-block-library-theme' );
+        wp_dequeue_style( 'wc-block-style' ); // Remove WooCommerce block CSS
+    }
+    
+    add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
