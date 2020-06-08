6e302d6aa72a51dd7c5b6cf42c3c6b2307ea95fa
Alexandr <aleksandr.zanko@enway.com>
2020-06-08 09:35:04 +0300

    remove embed

--- core/init_theme.php
+++ core/init_theme.php
@@ -48,5 +48,11 @@
         wp_dequeue_style( 'wp-block-library-theme' );
         wp_dequeue_style( 'wc-block-style' ); // Remove WooCommerce block CSS
     }
-    
+
     add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
+
+    function my_deregister_scripts(){
+        wp_deregister_script( 'wp-embed' );
+    }
+    
+    add_action( 'wp_footer', 'my_deregister_scripts' );
