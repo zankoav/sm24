1b9db8543a7a11ae3e437c006a2dee4c71f38362
Aleksandr.Zanko <Aleksandr.Zanko@fleetcor.com>
2023-02-07 11:41:29 +0300

    Added GTM

--- header.php
+++ header.php
@@ -10,7 +10,20 @@
     <meta name="viewport"
           content="initial-scale=1.0, width=device-width">
     <meta charset="<?php bloginfo( 'charset' ); ?>">
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KR7B7V9');</script>
+    <!-- End Google Tag Manager -->
+    
     <?php wp_head(); ?>
 </head>
 
 <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KR7B7V9"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
